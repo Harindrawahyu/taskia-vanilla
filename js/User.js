d17ea7baadd7f91036e0e797aa2cb0b59d038d60
harindra <harindrawahyu131@gmail.com>
2025-08-12 15:46:20 +0700

    modified: constructor in User class model
    penambahan logic pencegahan duplicate pada username, logic push data ke local storage

--- js/User.js
+++ js/User.js
@@ -1,52 +1,54 @@
 // User model
 
 class User {
-    constructor() {
-        this.users = this.getUsers() || [];
+  constructor() {
+    this.users = this.getUsers() || [];
+  }
+
+  saveUser(userData) {
+    const newUser = {
+      id: Date.now(),
+      username: userData,
+    };
+    // Cek jika username sudah ada → hindari duplikasi
+    const isDuplicate = this.users.some(
+      (user) => user.username.toLowerCase() === newUser.username.toLowerCase()
+    );
+    if (isDuplicate) {
+      return {
+        success: false,
+        message: "Username sudah terdaftar!",
+      };
     }
+    // push data ke local storage
+    this.users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(this.users));
 
-    saveUser(userData) {
-        const newUser = {
-            id: Date.now(),
-            username: userData,
-        };
-        // Cek jika username sudah ada → hindari duplikasi
-        const isDuplicate = this.users.some(user => user.username.toLowerCase() === newUser.username.toLowerCase());
-        if (isDuplicate) {
-            return {
-                success: false,
-                message: 'Username sudah terdaftar!',
-            };
-        }
-        // push data ke local storage
-        this.users.push(newUser);
-        localStorage.setItem("users", JSON.stringify(this.users));
+    return {
+      success: true,
+    };
+  }
 
-        return {
-            success: true,
-        };
+  loginUser(usernameByInput) {
+    const userExists = this.users.some(
+      (user) => user.username.toLowerCase() === usernameByInput.toLowerCase()
+    );
+
+    if (userExists) {
+      return {
+        success: true,
+        usernameByInput,
+      };
+    } else {
+      return {
+        success: false,
+        message:
+          "Username kamu belum terdaftar, Silahkan daftar terlebih dahulu",
+      };
     }
+  }
 
-    loginUser(usernameByInput) {
-        const userExists = this.users.some(user => user.username.toLowerCase() === usernameByInput.toLowerCase());
-
-        if (userExists) {
-            return {
-                success: true,
-                usernameByInput
-            }
-        }
-        else {
-            return {
-                success: false,
-                message: 'Username kamu belum terdaftar, Silahkan daftar terlebih dahulu',
-
-            };
-        }
-    }
-
-
-    getUsers() {
-        return JSON.parse(localStorage.getItem("users")) || [];
-    }
+  getUsers() {
+    return JSON.parse(localStorage.getItem("users")) || [];
+  }
 }
